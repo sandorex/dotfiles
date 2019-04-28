1918084121d43c7d1e6cba5cb367dcdd9e0b7ae6
Alex <rzhw3h@gmail.com>
2019-04-28 16:29:34 +0000

    Removing extension cause there is builtin support for it

--- user/vscode/install-packages.sh
+++ user/vscode/install-packages.sh
@@ -6,7 +6,6 @@ code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension reblws.one-dark-flatland-monokai
 code --install-extension ritwickdey.LiveServer
-code --install-extension shardulm94.trailing-spaces
 code --install-extension sidneys1.gitconfig
 code --install-extension twxs.cmake
 code --install-extension vector-of-bool.cmake-tools
