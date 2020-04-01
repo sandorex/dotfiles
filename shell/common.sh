0e2e64e1c282c0e1fc924357b145f7b081501c2e
Sandorex <rzhw3h@gmail.com>
2020-04-01 14:46:23 +0200

    Simplifying shell configs

--- shell/common.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# enable full color support
-export TERM=xterm-256color
--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -1,16 +1,14 @@
 #!/usr/bin/env zsh
 
 # download zgen if not already present
-[ -f "$HOME/.zgen/zgen.zsh" ] || git clone https://github.com/tarjoilija/zgen.git "$HOME/.zgen"
+[ -f "$HOME"/.zgen/zgen.zsh ] || git clone https://github.com/tarjoilija/zgen.git "$HOME"/.zgen
 
 # load zgen
-source "$HOME/.zgen/zgen.zsh"
+source "$HOME"/.zgen/zgen.zsh
 
 # get plugins and stuff
 if ! zgen saved; then
    zgen oh-my-zsh plugins/sudo
-
-   zgen load denysdovhan/spaceship-prompt spaceship
 
    zgen load zsh-users/zsh-autosuggestions
 
@@ -19,19 +17,3 @@ if ! zgen saved; then
 
    zgen save
 fi
-
-# plugin configuration #
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_GIT_BRANCH_SUFFIX=" "
-SPACESHIP_EXEC_TIME_ELAPSED=5
-
-SPACESHIP_PROMPT_ORDER=(
-   dir
-   git_branch
-   exec_time
-   line_sep
-   host
-   char
-)
-
-SPACESHIP_RPROMPT_ORDER=(jobs)
