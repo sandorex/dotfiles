8d6cb92b1272611686b8232e115d3cbaf044111c
Sandorex <rzhw3h@gmail.com>
2020-04-26 20:02:23 +0200

    Adding the xinit script, fixing dracula theme, fixing a typo in bashrc

---
+++ dot_shell/xinit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# TODO run irexec-user if not running
+
+# map caps to new modifier hyper
+setxkbmap -option caps:hyper
+xmodmap -e "remove mod4 = Hyper_L"
+xmodmap -e "add mod3 = Hyper_L"
+
+# disable bell
+xset b off
+
+# set key repeat speed
+# for plasma check "Leave Unchanged" in Keybord > Keyboard Repeat
+xset r rate 350 30
